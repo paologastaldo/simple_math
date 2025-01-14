ae3474c748495aa96c3d1f8abc47efa48b5ab4a2
PaoloGastaldo <paolo.gastaldo@unige.it>
2025-01-14 16:05:20 +0100

    first version of the library - no tests

---
+++ simple_math.c
@@ -0,0 +1,11 @@
+/** \file simple_math.c
+	\brief definition of the functions 
+	\author Paolo Gastaldo
+  
+*/
+
+#include "simple_math.h"
+
+float mean(int a, int b) {
+	return (a+b)/2;
+}
---
+++ simple_math.h
@@ -0,0 +1,13 @@
+/** \file simple_math.h
+	\brief a toy example of math library
+	\author Paolo Gastaldo
+  
+*/
+
+
+#ifndef _SIMPLEMATH_ 
+#define _SIMPLEMATH_
+
+float mean (int a, int b);
+
+#endif
